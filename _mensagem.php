b5a7ce10eb87316fd5a866273cd6bd8fd10d9a7b
Fernando Aranha <ferspider3@hotmail.com>
2017-07-23 19:16:54 -0400

    Correção
    Corrigido erro do Modal que estava sobreposto a direita

--- _mensagem.php
+++ _mensagem.php
@@ -1,32 +1,19 @@
 <?php if(isset($_GET['msg'])){ ?>
-  <!-- Example Modal 4: Corresponds with Portfolio Carousel Item 4 -->
-  <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-hidden="true")">
-      <div class="modal-dialog">
-          <div class="modal-content">
-              <div class="close-modal" data-dismiss="modal">
-                  <div class="lr">
-                      <div class="rl">
-                      </div>
-                  </div>
-              </div>
-              <div class="modal-body">
-                  <div class="container">
-                      <div class="row">
-                          <div class="col-lg-8 offset-lg-2">
-                              <h2><?php print $_GET['msg']; ?></h2>
-                              <hr class="colored">
-                          </div>
-                          <div class="col-lg-8 offset-lg-2">
-                              <ul class="list-inline item-details">
-                                  <li><strong>Obrigado por utilizar nossos Serviços!</strong>
-                                  </li>
-                              </ul>
-                              <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Fechar</button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+  <!-- Modal -->
+  <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="Modal">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title text-center" id="Modal">Mensagem</h4>
+        </div>
+        <div class="modal-body">
+          <h2 class="text-center"><?php print $_GET['msg']; ?></h2>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+        </div>
       </div>
     </div>
+  </div>
 <?php } ?>
